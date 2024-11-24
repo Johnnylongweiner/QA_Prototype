234804a31be560f16e08508d7f5a72c5c85e3b1f
Johnnylongweiner <john.inez787@gmail.com>
2024-11-24 19:19:47 +0800

    OOP

---
+++ Assets/Abstraction/Mercenary.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mercenary : NPCStats
+{
+    private void Start()
+    {
+        Movement();
+        TookDamage();
+    }
+    protected override void TookDamage()
+    {
+        Debug.Log("Mercenary hits Archer");
+    }
+    protected override void Movement()
+    {
+        Debug.Log("Mercenary dips");
+    }
+}
+
---
+++ Assets/Abstraction/NPC Stats.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCStats : MonoBehaviour
+{
+    [SerializeField] protected int health;
+    [SerializeField] protected int attack;
+    [SerializeField] protected float speed;
+    [SerializeField] protected int defense;
+
+    protected abstract void Movement();
+
+    protected abstract void TookDamage();
+}
+
